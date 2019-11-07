8a1875f77b0e2f1bc9d2c9e35e8a611c94bdec55
System Administrator <robin.kelmen@my.wheaton.edu>
2019-11-07 15:14:11 +0300

    Adding the php folder with basic php files

---
+++ PHP_files/constants.php
@@ -0,0 +1,12 @@
+<?php 
+
+	
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', 'root');
+	define('DB_NAME', 'Plantnursery');
+	define('DB_PORT', 3306);
+	define('DB_SOCKET', "/Applications/MAMP/tmp/mysql/mysql.sock");
+
+	?>
+	
